e7526da8d9837015c8956fec7de31d4c0b0b69a8
Carlos Ruiz <xarlybovi@gmail.com>
2021-08-06 17:48:52 +0200

    test: add cached request coverage

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -67,5 +67,22 @@ describe Halo::Client do
     it 'instantiates a redis client if redis is in the options' do
       expect(subject.instance_variable_get(:@redis)).to be_a(Redis)
     end
+
+    it 'check if a request is cached properly' do
+      client = Halo.halo5
+      # First request creates the cache entry
+      response_vcr = VCR.use_cassette('halo5/metadata/campaign_missions') { client.metadata.campaign_missions }
+      # Second request get it from redis cache
+      response_cached = client.metadata.campaign_missions
+
+      # Get the redis cache entry from the redis client
+      redis_cache_entry = client.cache_store[:redis].get('/metadata/h5/metadata/campaign-missions{}en')
+      redis_cache_entry = JSON.parse(redis_cache_entry)
+
+      expect(response_vcr).to eq(response_cached)
+      expect(response_cached).to eq(redis_cache_entry)
+      expect(response_cached).to be_kind_of(Array)
+      expect(response_cached.first).to have_key 'id'
+    end
   end
 end
